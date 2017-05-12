09dc4b6c93edd2b9f5aaaa3794e3e38e7fb720d8
sunjie <544345498@qq.com>
2017-05-12 16:05:36 +0800

    左导航页面及数据

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,5 +1,6 @@
 <?php
 namespace app\index\controller;
+use app\index\model\AuthRule;
 use app\index\model\User;
 use think\View;
 
@@ -7,6 +8,13 @@ class Index extends \think\Controller
 {
     public function index()
     {
+        $model_rule = new AuthRule();
+        $map = array();
+        $map['is_menu'] = AuthRule::IS_MENU;
+        $rule_list = $model_rule->getRuleList($map);
+        $this->assign("rule_list",$rule_list);
+//        dump($rule_list[1]['child']);die;
+//        dump($rule_list[1]['child']);die;
         return $this->fetch();
     }
 
---
+++ application/index/model/AuthRule.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\index\model;
+use think\Model;
+class AuthRule extends Model
+{
+    protected $tableName = 'auth_rule';
+
+    const IS_MENU = 'Y';
+    const IS_NOT_MENU = 'N';
+
+    /**
+     * 获取左导航列表
+     * @param $map
+     * @return false|\PDOStatement|string|\think\Collection
+     */
+    public function getRuleList($map)
+    {
+        $rule_list = db($this->tableName)->where($map)->select();
+        if($rule_list){
+            foreach($rule_list as $key=>&$rule){
+                $map_get_child = array();
+                $map_get_child['pid'] = $rule['id'];
+                $rule['child'] = db($this->tableName)->where($map_get_child)->select();
+            }
+        }
+        return $rule_list;
+    }
+}
