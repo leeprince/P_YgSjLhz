aa327911f2ebaffaad03f2052da0650d05d05e2d
ygtomorrow <1228946196@qq.com>
2017-05-13 11:29:17 +0800

    登录界面及css img文件夹

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -37,4 +37,8 @@ class Index extends Controller
         $this->assign("user_list",$user_list);
         return $this->fetch();
     }
+    public function login()
+    {
+        return $this->fetch();
+    }
 }
